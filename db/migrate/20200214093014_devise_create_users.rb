e9e95866ef03e0fb27f843c7e31950e4f2679353
kaznakamata <kazyokoyama8754@gmail.com>
2020-02-15 11:32:36 +0900

    edit devise user migration file

--- db/migrate/20200214093014_devise_create_users.rb
+++ db/migrate/20200214093014_devise_create_users.rb
@@ -4,8 +4,19 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :nickname,           null: false, default: ""
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :first_name
+      t.string :last_name
+      t.string :first_name_kana
+      t.string :last_name_kana
+      t.integer :birthday_yyyy
+      t.integer :birthday_mm
+      t.integer :birthday_dd
+      t.integer :address_id
+      t.integer :card_id
+      t.text :text
 
       ## Recoverable
       t.string   :reset_password_token
@@ -36,6 +47,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.timestamps null: false
     end
 
+    add_index :users, :nickname,             unique: true
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
