9d93ec36bb6f0868799a1ddbfef7ff9e3abebb43
nakatanikodai <nakatanixx1123@gmail.com>
2020-02-16 14:18:56 +0900

    #categoryモデルの実装（コンフリク修正版）

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_15_133049) do
+ActiveRecord::Schema.define(version: 2020_02_15_072207) do
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "image"
