2b063906ff5fdd84049d5cd02624c58ce0f4f632
momokaisshiki <nanomugen69@gmail.com>
2020-02-23 16:46:26 +0900

    users_edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "products#index"
-  resources :users
+  resources :users, only: [:edit, :update]
   resources :products
   resources :categories
 end
