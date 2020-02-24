8759d20f5d4e584e469103a08ad2bbf085a31f21
momokaisshiki <nanomugen69@gmail.com>
2020-02-24 20:09:49 +0900

    メインカテゴリーテーブル削除

--- db/migrate/20200224084522_add_ancestry_to_maincategory.rb
+++
@@ -1,6 +0,0 @@
-class AddAncestryToMaincategory < ActiveRecord::Migration[5.2]
-  def change
-    add_column :maincategories, :ancestry, :string
-    add_index :maincategories, :ancestry
-  end
-end
