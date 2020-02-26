442567b46d0848610df64113ef96bcb38a34787f
yamatog <57852607+yamatog@users.noreply.github.com>
2020-02-26 17:12:44 +0900

    修正点の改修

---
+++ app/assets/javascripts/product/new.js
@@ -0,0 +1,19 @@
+function previewImage(obj){
+  var fileReader = new FileReader();
+  fileReader.onload = (function() {
+    document.getElementById('preview').src = fileReader.result;
+  });
+  fileReader.readAsDataURL(obj.files[0]);
+}
+
+function calcProfit() {
+  var price = document.getElementById('price__num').value;
+  price = Number(price);
+  var com = price * 0.1;
+  document.getElementById('commission__num').innerHTML = "¥ " + com;
+
+  var price = document.getElementById('price__num').value;
+  price = Number(price);
+  var profit = price * 0.9;
+  document.getElementById('profit__num').innerHTML = "¥ " + profit;
+}
