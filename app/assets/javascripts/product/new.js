5bd62201bb0b98dbb3aad5536b61077bc140c97e
yamatog <57852607+yamatog@users.noreply.github.com>
2020-02-27 12:58:18 +0900

    Update new.js

--- app/assets/javascripts/product/new.js
+++ app/assets/javascripts/product/new.js
@@ -9,10 +9,10 @@ function previewImage(obj){
 function calcProfit() {
   var price = document.getElementById('price__num').value;
   price = Number(price);
+  
   var com = price * 0.1;
   document.getElementById('commission__num').innerHTML = "¥ " + com;
 
-  price = Number(price);
   var profit = price * 0.9;
   document.getElementById('profit__num').innerHTML = "¥ " + profit;
 }
