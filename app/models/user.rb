ddb0a9788cdd3a1bae4dc27428e0553db734bc93
momokaisshiki <nanomugen69@gmail.com>
2020-02-27 20:07:41 +0900

    validates test
    validates test

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :cards
   has_many :products
+  validates :nickname, :password, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday_yyyy, :birthday_mm, :birthday_dd, presence: true
+  validates :password, length: { minimum: 7 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  validates :kind, inclusion: { in: %w(draft publish private) }
+  VALID_NAME_KANA = /^[ア-ン゛゜ァ-ォャ-ョー「」、]+$/
+  validates :first_name_kana, :last_name_kana, presence: true, uniqueness: true, format: { with: VALID_NAME_KANA }
+  VALID_NAME = /^[ぁ-んァ-ヶー一-龠]+$/
+  validates :first_name, :last_name, presence: true, uniqueness: true, format: { with: VALID_NAME }
+
 end
