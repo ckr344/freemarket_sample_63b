3ecf7cb933dbba7dad724067943bd0613da870fc
yamatog <57852607+yamatog@users.noreply.github.com>
2020-03-05 18:32:11 +0900

    商品詳細画面と編集機能の実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,8 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!, except:[:index]
-
+  before_action :authenticate_user!, except:[:index, :show]
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  
   def index
     
   end
@@ -19,24 +21,43 @@ class ProductsController < ApplicationController
   end
 
   def show
-    
+    @product = Product.find(params[:id])
   end
 
   def edit
-    
+    @product = Product.find(params[:id])
   end
 
   def update
-    
+    @product = Product.find(params[:id])
+    if @product.user_id == current_user.id
+      @product.update(product_params)
+      redirect_to product_path
+    else
+      render 'edit'
+    end
   end
 
   def destroy
-    
+    if @product.user_id == current_user.id
+      @product.destroy
+      redirect_to root_path
+    else
+      render 'show'
+    end
   end
 
   private
   def product_params
-    params.require(:product).permit(:name, :description, :status, :delivery_charge, :delivery_method, :delivery_prefecture, :delivery_days, :size, :brand, :price, :transaction_id, :main_category_id, :second_category_id, :third_category_id).merge(user_id: current_user.id)
+    params.require(:product).permit(:name, :description, :status, :delivery_charge, :delivery_method, :delivery_prefecture, :delivery_days, :size, :brand, :price, :transaction_id, :main_category_id, :second_category_id, :third_category_id, :user_id).merge(user_id: current_user.id)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def set_user
+    @user = User.find(@product.user_id)
   end
 
 end
