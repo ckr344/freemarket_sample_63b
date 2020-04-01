1279f85abb39ddfc0d5f6051f0b95921a8eabc44
ckr344 <ckr344.225@gmail.com>
2020-04-01 20:20:03 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -54,12 +54,29 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
+# master keyの値を取ってくるために必要
+append :linked_files, 'config/master.key'
+
 # デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
+
+  # Rails5.2以上（credentials.yml）の場合に必要な記載
+  desc 'upload master.key'
+  task :upload do
+  on roles(:app) do |host|
+  if test "[ ! -d #{shared_path}/config ]"
+  execute "mkdir -p #{shared_path}/config"
+  end
+  upload!('config/master.key', "#{shared_path}/config/master.key")
+  end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
+  # ここまで
 end
 
 set :default_env, {
