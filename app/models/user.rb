2dc932ee9be3b2c3f8eabcfb5d162b319b0bffdd
ckr344 <ckr344.225@gmail.com>
2020-04-01 13:21:05 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :cards
   has_many :products
-  has_many :transactions
+  has_many :trade, class_name:"transaction"
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   VALID_NAME_KANA = /\A[ア-ン゛゜ァ-ォャ-ョー「」、]+\z/
   VALID_NAME = /\A[ぁ-んァ-ヶー一-龠]+\z/
